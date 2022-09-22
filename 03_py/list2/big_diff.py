73c508af981daa1ac49e21fd3834b81ec987796e
dchen278 <dchen30@stuy.edu>
2022-09-22 13:09:15 -0400

    krewes

--- 03_py/list2/big_diff.py
+++ 03_py/list2/big_diff.py
@@ -1,2 +1,2 @@
 def big_diff(nums):
-  return max(nums) - min(nums)
+  return max(nums) - min(nums)dsada
---
+++ 04_choose/krewes.py
@@ -0,0 +1,32 @@
+""" 
+David Chen, Diana Akhmedova, Sam Cowan
+SoftDev
+04_choose
+2022-09-22
+time spent: 
+
+DISCO: 
+ - random.choice returns a random element from given sequence
+ - dictionary stores keys and values and allows us to reference them by key
+
+QCC:
+ - are these pseudorandom?
+ - what does pseudorandom numbers entail?
+
+OPS SUMMARY:
+ - we used random from python to return a devo from krewes
+"""
+
+import random
+
+krewes = {2: ["hwang30", "wliu30", "iyeung30"],
+          7: ["dchen30", "dakhmedova30", "scowan30"],
+          8: ["sho30", "dhe30", "mmori30"]
+          }
+
+def chooseDevo():
+    randomPd = random.choice([2,7,8])
+    data = krewes[randomPd]
+    return data[random.randint(0, len(data) - 1)]
+
+print(chooseDevo())
