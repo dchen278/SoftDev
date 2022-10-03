928cb63d1b927ba37e3c675ae735e625295b1712
dchen278 <dchen30@stuy.edu>
2022-10-03 12:53:22 -0400

    uh fixed numbercruncher

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -13,9 +13,8 @@ QCC:
     - N/A
 
 """
-from operator import indexOf
+import enum
 import random
-import math
 
 with open("./occupations.csv", 'r', encoding='utf8') as f:
     occupation_data = f.read()
@@ -30,18 +29,18 @@ def comma_in_field(row):
     data = row.split('"')
     # print(data)
     percentage = row.split(",") 
-    print(percentage[-1])
     return [data[1], percentage[-1]]
 
-for occupation in occupation_list:
-    if '"' in occupation:
-        data_in_comma = comma_in_field(occupation)
-        occupation_classes.append(data_in_comma[0])
-        occupation_percentages.append(data_in_comma[1])
-    else:
-        data = occupation.split(",")
-        occupation_classes.append(data[0])
-        occupation_percentages.append(data[1])
+for index, occupation in enumerate(occupation_list):
+    if index != 0 and index != len(occupation_list) - 1:
+        if '"' in occupation:
+            data_in_comma = comma_in_field(occupation)
+            occupation_classes.append(data_in_comma[0])
+            occupation_percentages.append(float(data_in_comma[1]))
+        else:
+            data = occupation.split(",")
+            occupation_classes.append(data[0])
+            occupation_percentages.append(float(data[1]))
 
 # remove the labels
 occupation_classes.pop(0)
@@ -49,11 +48,9 @@ occupation_percentages.pop(0)
 occupation_classes.pop(len(occupation_classes)-1)
 occupation_percentages.pop(len(occupation_percentages)-1)
 
-
+# print(len(occupation_classes))
+# print(len(occupation_percentages))
 # print(occupation_classes)
-print(len(occupation_classes))
 # print(occupation_percentages)
-print(len(occupation_percentages))
 
-
-print(random.choices(occupation_classes,weights=occupation_percentages))
+print(random.choices(occupation_classes,weights=occupation_percentages)[0])
