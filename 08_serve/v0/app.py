f3c07247f2bd882ffb27ef90c20f3471ca34db83
dchen278 <dchen30@stuy.edu>
2022-10-04 13:26:30 -0400

    dabjl

--- 08_serve/v0/app.py
+++ 08_serve/v0/app.py
@@ -1,9 +1,9 @@
-# Clyde 'Thluffy' Sinclair
+# David Chen, Kevin Li, 
 # SoftDev
 # Oct 2022
 
 from flask import Flask
-app = Flask(__name__) # ...
+app = Flask(__name__) # create an instance of our flask app
 
 @app.route("/") # ...
 def hello_world():
