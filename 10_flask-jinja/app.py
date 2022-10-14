fcb41e0a91cbac95ad228ff6d70f48aaaddfad59
David Chen <dchen30@stuy.edu>
2022-10-14 02:03:52 -0400

    modify heading for hw 10

--- 10_flask-jinja/app.py
+++ 10_flask-jinja/app.py
@@ -1,23 +1,31 @@
-# Karen Shekyan
+# David Chen
 # SoftDev
 # Oct 13, 2022
 
-from flask import Flask, render_template #Q0: What will happen if you remove render_template from this line? (log your prediction before you pull the trigger...)
+# Q0: What will happen if you remove render_template from this line? (log your prediction before you pull the trigger...)
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "No hablo queso!"
 
-coll = [0,1,1,2,3,5,8]
 
-@app.route("/my_foist_template") #Q1: Can all of your teammates confidently predict the URL to use to load this page?
+coll = [0, 1, 1, 2, 3, 5, 8]
+
+
+# Q1: Can all of your teammates confidently predict the URL to use to load this page?
+@app.route("/my_foist_template")
 def test_tmplt():
-    return render_template( 'model_tmplt.html', foo="fooooo", collection=coll) #Q2: What is the significance of each argument? Simplest, most concise answer best.
+    # Q2: What is the significance of each argument? Simplest, most concise answer best.
+    return render_template('model_tmplt.html', foo="fooooo", collection=coll)
+
 
 @app.route("/foo")
 def test():
-    return render_template("foo.html");
+    return render_template("foo.html")
+
 
 if __name__ == "__main__":
     app.debug = True
