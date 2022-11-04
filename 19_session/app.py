813070a51146f73d11403f90cd5849df66cceb0e
dchen278 <dchen30@stuy.edu>
2022-11-04 13:19:36 -0400

    Finish app.py

--- 19_session/app.py
+++ 19_session/app.py
@@ -6,7 +6,7 @@
 
 from flask import Flask  # facilitate flask webserving
 from flask import render_template  # facilitate jinja templating
-from flask import request, Response, make_response  # facilitate form submission
+from flask import request, Response, redirect, session, url_for  # facilitate form submission
 
 # the conventional way:
 #from flask import Flask, render_template, request
@@ -14,6 +14,7 @@ username = "DWM"
 password = "ABC"
 
 app = Flask(__name__)  # create Flask object
+app.secret_key = "m4Wa0SY66R34R2fbty7P5Nmxg8fLNOQ6"
 
 @app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
@@ -28,38 +29,53 @@ def disp_loginpage():
     # print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    if request.cookies.get("username") == username:
-        return render_template('response.html', status="Signed in")
+    if "username" in session:
+        return render_template('response.html', status="Successful")
     else:
         return render_template('login.html')
 
 
 @app.route("/auth", methods=['GET', 'POST'])
 def authenticate():
+    if request.method == "GET":
+        if "username" in session:
+            return render_template('response.html', status="Successful")
+        else:
+            return render_template('login.html')
+    elif request.method == "POST":
+        print("\n\n\n")
+        print("***DIAG: this Flask obj ***")
+        print(app)
+        print("***DIAG: request obj ***")
+        print(request)
+        print("***DIAG: request.args ***")
+        print(request.form)  # displays entered info as dict
+        print("***DIAG: request.args['username']  ***")
+        print(request.form['username'])
+        print("***DIAG: request.headers ***")
+
+        if request.form['username'] == username and request.form['password'] == password:
+            session["username"] = request.form.get("username")
+            return render_template('response.html', status="Successful")
+        elif request.form['username'] != username and request.form['password'] != password:
+            return render_template('response.html', status="Incorrect Username and Password")
+        elif request.form['username'] != username:
+            return render_template('response.html', status="Incorrect Username")
+        elif request.form['password'] != password:
+            return render_template('response.html', status="Incorrect Password")
+        return f"Waaaa hooo HAAAH {request.form['username']}"  # response to a form submission
+    else:
+        return Response(status=405)
+
+@app.route("/logout", methods=['POST'])
+def logout():
     if request.method != "POST":
         return Response(status=405)
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
     print(request.form)  # displays entered info as dict
-    print("***DIAG: request.args['username']  ***")
-    print(request.form['username'])
-    print("***DIAG: request.headers ***")
-    # metadata for the server about request+machine requesting
-    if request.form['username'] == username and request.form['password'] == password:
-        resp = make_response(render_template('response.html', status="Successful"))
-        resp.set_cookie("username", username)
-        return resp
-    elif request.form['username'] != username and request.form['password'] != password:
-        return render_template('response.html', status="Incorrect Username and Password")
-    elif request.form['username'] != username:
-        return render_template('response.html', status="Incorrect Username")
-    elif request.form['password'] != password:
-        return render_template('response.html', status="Incorrect Password")
-    return f"Waaaa hooo HAAAH {request.form['username']}"  # response to a form submission
+    if "username" in session:
+        session.pop("username")
+    return redirect("/auth")
+
 
 
 if __name__ == "__main__":  # false if this file imported as module
---
+++ 19_session/readme.md
@@ -0,0 +1,9 @@
+# Disturbed Window Monsters
+# K19 - Session Greetings
+### Roster: David Chen, William Guo, Marc Jiang
+### SoftDev
+### Nov 4 2022
+### Time Spent: 1.5
+
+## DISCO
+- 
